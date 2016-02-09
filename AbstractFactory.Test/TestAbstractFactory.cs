e0f81fb303a52560b509911ff71ed2ecd4ceaf58
esouzagithub <eduardo.techhouse@gmail.com>
2016-02-09 12:27:08 -0200

    Example abstract factory C#
    Example abstract factory database

--- AbstractFactory.Test/TestAbstractFactory.cs
+++ AbstractFactory.Test/TestAbstractFactory.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
 
 namespace AbstractFactory.Test
 {
@@ -9,14 +10,34 @@ namespace AbstractFactory.Test
         [TestMethod]
         public void Test_Type_SQLServerDatabase()
         {
+            // Define tipo Sql
             AbstractDataBase dataBase = new SqlServer();
+            // Atribuindo command
+            DbCommand cmd = dataBase.Command;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT, DELETE, INSERT, UPDATE";
+            cmd.Connection.Open();
+            DbDataReader reader = cmd.ExecuteReader();
+            reader.Close();
+            cmd.Connection.Close();
+
             Assert.IsInstanceOfType(dataBase, typeof(SqlServer));
         }
 
         [TestMethod]
         public void Test_Type_OleDbServerDatabase()
         {
+            // Define tipo OleDb
             AbstractDataBase dataBase = new OleDb();
+            // Atribuindo command
+            DbCommand cmd = dataBase.Command;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT, DELETE, INSERT, UPDATE";
+            cmd.Connection.Open();
+            DbDataReader reader = cmd.ExecuteReader();
+            reader.Close();
+            cmd.Connection.Close();
+
             Assert.IsInstanceOfType(dataBase, typeof(OleDb));
         }
     }
